ba366aac8694978d1db632cf77c9a85d22878d60
anywhere3D <anywhere3d@gmail.com>
2018-07-01 19:41:54 +0200

    Update imgurUpload.js

--- imgurUpload.js
+++ imgurUpload.js
@@ -1,6 +1,33 @@
 //  imgurUpload.js
 
-function uploadToImgur(file, callback) {
+function uploadToImgur(file, callback){
+    return new Promise( function(resolve, reject){
+        var clientid = "95e1ecaf14ca5e6";
+        var endpoint = "https://api.imgur.com/3/image";
+        var formdata = new FormData();
+        formdata.append("image", file);
+        var xhttp = new XMLHttpRequest();
+        xhttp.open('POST', endpoint, true);
+        xhttp.setRequestHeader('Authorization', 'Client-ID ' + clientid);
+        xhttp.onreadystatechange = function () {
+            if (this.readyState === 4) {
+                var response = "";
+                if (this.status >= 200 && this.status < 300) {
+                    response = JSON.parse(this.responseText);
+                    if (!!callback) callback.call(window, response);
+                    resolve( response );
+                } else {
+                    var error = this.responseText;
+                    throw Error( error );
+                }
+            }
+        };
+        xhttp.send(formdata);
+        xhttp = null;
+    });
+}
+
+function uploadToImgurXHR(file, callback) {
 
     var clientid = "95e1ecaf14ca5e6";
     var endpoint = "https://api.imgur.com/3/image";
