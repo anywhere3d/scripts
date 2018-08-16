ce2c429d205b8e29516b4aec51503cfced9d654a
anywhere3D <anywhere3d@gmail.com>
2018-08-16 19:17:19 +0200

    Delete getDecodedItemFromLocalStorage.js

--- getDecodedItemFromLocalStorage.js
+++
@@ -1,6 +0,0 @@
-//  public/scripts/getDecodedItemFromLocalStorage.js
-//  requires "/js/rawinflate.js".
-
-    function getDecodedItemFromLocalStorage( key ){
-        return JSON.parse( RawDeflate.inflate( window.atob( localStorage.getItem( key ) ) ) );
-    }
