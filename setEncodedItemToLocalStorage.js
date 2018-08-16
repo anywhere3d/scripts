9c282d4281e63fd6a5af3a5b5db0d9446f0811cb
anywhere3D <anywhere3d@gmail.com>
2018-08-16 19:17:27 +0200

    Delete setEncodedItemToLocalStorage.js

--- setEncodedItemToLocalStorage.js
+++
@@ -1,6 +0,0 @@
-//  public/scripts/setEncodedItemToLocalStorage.js
-//  requires "/js/rawdeflate.js".
-
-    function setEncodedItemToLocalStorage(key, data){
-        localStorage.setItem(key, window.btoa( RawDeflate.deflate( JSON.stringify( data ) ) ) );
-    }
